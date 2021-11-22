2513d3fa579c148add35c06275d52495f83b6bdc
Dandav2923 <daniele.emili23@gmail.com>
2021-11-22 17:32:07 +0100

    fine

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const element = document.querySelector('.header-right > a');
 const elementAttivo = document.querySelector('header .hamburger-menu');
 const iconCross = document.querySelector ('.hamburger-menu > a');
-
+ 
 element.addEventListener('click',
 function() {
     elementAttivo.classList.add('active');
